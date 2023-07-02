858b514bf0c896b5097bdaced147f11dbd547d97
Brian Floersch <gh123man@gmail.com>
2023-07-02 16:36:29 -0400

    Initial Commit

---
+++ ImageScrollView/ImageScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageScrollViewApp.swift
+//  ImageScrollView
+//
+//  Created by Brian Floersch on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageScrollViewUITests/ImageScrollViewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ImageScrollViewUITestsLaunchTests.swift
+//  ImageScrollViewUITests
+//
+//  Created by Brian Floersch on 7/2/23.
+//
+
+import XCTest
+
+final class ImageScrollViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
