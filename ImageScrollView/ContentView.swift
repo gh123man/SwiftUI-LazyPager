0e2cd7fbd84dee29ed1c3dcca6643dfa88975499
Brian Floersch <gh123man@gmail.com>
2023-07-04 19:00:08 -0400

    paging working

--- ImageScrollView/ContentView.swift
+++ ImageScrollView/ContentView.swift
@@ -50,29 +50,7 @@ struct ZoomableScrollView<Content: View, BottomContent: View, DataType>: UIViewR
     }
 
     func makeUIView(context: Context) -> UIScrollView {
-        
         return context.coordinator.setupScrollView()
-        
-//        // create a UIHostingController to hold our SwiftUI content
-//        guard let hostedView = context.coordinator.hostingController.view else {
-//            return scrollView
-//        }
-//        hostedView.translatesAutoresizingMaskIntoConstraints = false
-//        hostedView.backgroundColor = .clear
-//
-//        scrollView.addSubview(hostedView)
-//
-//        // Bottom content
-//        guard let bottomContent = context.coordinator.bottomController.view else {
-//            return scrollView
-//        }
-//
-//        bottomContent.translatesAutoresizingMaskIntoConstraints = false
-//
-//        scrollView.addSubview(bottomContent)
-//        context.coordinator.setupConstraints(scrollView)
-//
-//        return scrollView
     }
     
 
@@ -91,13 +69,10 @@ struct ZoomableScrollView<Content: View, BottomContent: View, DataType>: UIViewR
 
     class Coordinator: NSObject, UIScrollViewDelegate {
         private var viewLoader: (DataType) -> Content
-//        var hostingController: UIHostingController<Content>
-        var bottomController: UIHostingController<BottomContent>
         
         var data: [DataType]
         
         var scrollView = UIScrollView()
-        var contentView = UIView()
         
         var contentTopToFrame: NSLayoutConstraint!
         var contentTopToContent: NSLayoutConstraint!
@@ -106,35 +81,13 @@ struct ZoomableScrollView<Content: View, BottomContent: View, DataType>: UIViewR
         var currentView: UIView?
         var curretnIndex = 0
         
-        var allowScroll: Bool = true {
-            didSet {
-//                if allowScroll {
-//                    contentTopToContent.isActive = true
-//                    contentTopToFrame.isActive = false
-//                    contentBottomToFrame.isActive = false
-//                    bottomController.view.isHidden = false
-//                } else {
-//                    contentTopToContent.isActive = false
-//                    contentTopToFrame.isActive = true
-//                    contentBottomToFrame.isActive = true
-//                    bottomController.view.isHidden = true
-//                }
-            }
-        }
-        
         init(data: [DataType], viewLoader: @escaping (DataType) -> Content, bottomController: UIHostingController<BottomContent>) {
             self.data = data
             self.viewLoader = viewLoader
-//            self.hostingController = hostingController
-            self.bottomController = bottomController
         }
 
-        
         func setupScrollView() -> UIScrollView {
             scrollView.delegate = self
-            scrollView.maximumZoomScale = 10
-            scrollView.minimumZoomScale = 1
-            scrollView.bouncesZoom = true
             scrollView.showsVerticalScrollIndicator = false
             scrollView.showsHorizontalScrollIndicator = false
             scrollView.backgroundColor = .clear
@@ -143,19 +96,27 @@ struct ZoomableScrollView<Content: View, BottomContent: View, DataType>: UIViewR
             scrollView.layer.borderColor = CGColor.init(red: 0, green: 255, blue: 0, alpha: 1)
             scrollView.layer.borderWidth = 2
             
-            contentView.translatesAutoresizingMaskIntoConstraints = false
-            scrollView.addSubview(contentView)
-            
-            NSLayoutConstraint.activate([
-                contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-                contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-                contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-                contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            ])
-            
             loadView(at: 0)
             loadNext()
             return scrollView
+        }
+        
+        func makeZommableView(view: UIView) -> UIView {
+            let sv = UIScrollView()
+            sv.translatesAutoresizingMaskIntoConstraints = false
+            sv.delegate = self
+            sv.maximumZoomScale = 10
+            sv.minimumZoomScale = 1
+            sv.bouncesZoom = true
+            sv.addSubview(view)
+            
+            NSLayoutConstraint.activate([
+                view.leadingAnchor.constraint(equalTo: sv.leadingAnchor),
+                view.trailingAnchor.constraint(equalTo: sv.trailingAnchor),
+                view.widthAnchor.constraint(equalTo: sv.frameLayoutGuide.widthAnchor),
+                view.heightAnchor.constraint(equalTo: sv.frameLayoutGuide.heightAnchor),
+            ])
+            return sv
         }
         
         func loadView(at index: Int) {
@@ -168,16 +129,20 @@ struct ZoomableScrollView<Content: View, BottomContent: View, DataType>: UIViewR
             
             loadedContent.translatesAutoresizingMaskIntoConstraints = false
             loadedContent.backgroundColor = .clear
-            contentView.addSubview(loadedContent)
+            
+            let sv = makeZommableView(view: loadedContent)
+            scrollView.addSubview(sv)
             
             NSLayoutConstraint.activate([
-                loadedContent.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-//                loadedContent.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-                loadedContent.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
-                loadedContent.heightAnchor.constraint(equalTo: scrollView.frameLayoutGuide.heightAnchor),
+                
+                sv.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+//                sv.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+                sv.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+                sv.heightAnchor.constraint(equalTo: scrollView.frameLayoutGuide.heightAnchor),
+                
             ])
             
-            currentView = loadedContent
+            currentView = sv
         }
         
         func loadNext() {
@@ -193,75 +158,27 @@ struct ZoomableScrollView<Content: View, BottomContent: View, DataType>: UIViewR
             
             loadedContent.translatesAutoresizingMaskIntoConstraints = false
             loadedContent.backgroundColor = .clear
-            contentView.addSubview(loadedContent)
+            
+            let sv = makeZommableView(view: loadedContent)
+            scrollView.addSubview(sv)
             
             NSLayoutConstraint.activate([
-                loadedContent.leadingAnchor.constraint(equalTo: currentView.trailingAnchor),
-                loadedContent.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-                loadedContent.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
-                loadedContent.heightAnchor.constraint(equalTo: scrollView.frameLayoutGuide.heightAnchor),
+                sv.leadingAnchor.constraint(equalTo: currentView.trailingAnchor),
+                sv.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+                sv.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+                sv.heightAnchor.constraint(equalTo: scrollView.frameLayoutGuide.heightAnchor),
             ])
             
         }
         
-        func setupConstraints() {
-            
-//            NSLayoutConstraint.activate([
-//                hostingController.view.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
-//                hostingController.view.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
-//                hostingController.view.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
-//                hostingController.view.heightAnchor.constraint(equalTo: scrollView.frameLayoutGuide.heightAnchor),
-//            ])
-//
-//            contentTopToFrame = hostingController.view.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor)
-//            contentTopToContent = hostingController.view.topAnchor.constraint(equalTo: scrollView.topAnchor)
-//            contentBottomToFrame = hostingController.view.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor)
-//
-//
-//            NSLayoutConstraint.activate([
-//                bottomController.view.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-//                bottomController.view.topAnchor.constraint(equalTo: hostingController.view.bottomAnchor),
-//                bottomController.view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-//                bottomController.view.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-//                bottomController.view.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-//            ])
-//
-//            allowScroll = true
-        }
-        
-        func loadData() {
-            
-        }
-        
-        
-        func scrollViewDidScroll(_ scrollView: UIScrollView) {
-            updateState(scrollView)
-        }
-        
         func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-            return currentView
+            return scrollView.subviews[0]
         }
         
-        func updateState(_ scrollView: UIScrollView) {
-            
-//            allowScroll = scrollView.zoomScale == 1
-//
-//            if scrollView.contentOffset.y > 10 && scrollView.zoomScale == 1 {
-//                allowScroll = true
-//                scrollView.pinchGestureRecognizer?.isEnabled = false
-//            } else {
-//                scrollView.pinchGestureRecognizer?.isEnabled = true
-//            }
-        }
-        
-        func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
-            
-        }
         
         func scrollViewDidZoom(_ scrollView: UIScrollView) {
-            guard let imageView = currentView else {
-                return
-            }
+            let imageView = scrollView.subviews[0]
+            
             let w: CGFloat = imageView.intrinsicContentSize.width * UIScreen.main.scale
             let h: CGFloat = imageView.intrinsicContentSize.height * UIScreen.main.scale
 
@@ -283,7 +200,7 @@ struct ZoomableScrollView<Content: View, BottomContent: View, DataType>: UIViewR
 
             scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
 
-            updateState(scrollView)
+//            updateState(scrollView)
         }
     }
 }
